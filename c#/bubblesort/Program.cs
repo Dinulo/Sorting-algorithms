84c9822a525110471c53acb22fcbfe543e666e77
Dinulo <aleksandardinulovic99@gmail.com>
2021-09-28 17:56:16 +0200

    Create Program.cs
    Created Program.cs for C#

---
+++ c#/bubblesort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bubblesort \n");
+            Console.WriteLine("Please enter: ");
+            int size = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(size);
+
+        }
+    }
+}
