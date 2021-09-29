0657e416a6c42cd904b69fbf33946ff2ce76c402
Dinulo <aleksandardinulovic99@gmail.com>
2021-09-29 16:24:39 +0200

    Update Program.cs
    Added core functionality

--- c#/bubblesort/Program.cs
+++ c#/bubblesort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace bubblesort
 {
@@ -6,12 +7,56 @@ namespace bubblesort
     {
         static void Main(string[] args)
         {
+            /* To-do:
+            - validate inputs
+            - limit input to 65k or up to int32   
+             */
+            //get user inputs for range
             Console.WriteLine("Bubblesort \n");
-            Console.WriteLine("Please enter: ");
-            int size = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter array size (int): ");
+            int arraySize = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Enter bottom of range(int): ");
+            int rangeBottom = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Enter top of range(int): ");
+            int rangeTop = Int16.Parse(Console.ReadLine());
 
-            Console.WriteLine(size);
+            /*To-do:
+             - limit output to 100 chars or so if array is too long
+             */
+            //fill array with random numbers and print to console
+            Random rnd = new Random();
+            int[] numbers = new int[arraySize];
+            for (int i = 0; i < arraySize; i++) {
+                numbers[i] = rnd.Next(rangeBottom, rangeTop);
+            }
+            Console.WriteLine("Your random array: " + string.Join(",", numbers));
+            
+            //declare helping variables "swapped" and "slot" for swapping numbers and "stopwatch" for timing the execution
+            bool swapped = true;
+            int slot = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
 
+            while (swapped) {
+                swapped = false;
+                for (int i = 0; i < numbers.Length-1 ; i++) {
+                    if (numbers[i] > numbers[i + 1]) {
+                        slot = numbers[i];
+                        numbers[i] = numbers[i + 1];
+                        numbers[i + 1] = slot;
+                        swapped = true;
+                    }
+                }
+            }
+
+            /* To-do:
+             - proper exit and "run again" option
+             */
+            //stop time and display it with the sorted array
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            Console.WriteLine("Your sorted array: " + string.Join(",", numbers));
+            Console.WriteLine("Time elapsed: " + ts);
         }
     }
 }
