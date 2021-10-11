4cce0f61695bbd9adfae4f913df0dd7baa5edeec
Dinulo <aleksandardinulovic99@gmail.com>
2021-10-11 16:46:28 +0200

    Created Main.java

---
+++ java/bubblesort/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner inputReader = new Scanner(System.in);
+
+    static int parseStringToInt(String inputString){
+        int outputInt;
+        Boolean flag = true;
+        for(int a = 0; a<inputString.length(); a++) {
+
+        }
+
+        return outputInt;
+    }
+
+    public static void main(String[] args) {
+	System.out.println("Bubblesort");
+
+
+	System.out.println("Enter array size (int): ");
+
+
+	System.out.println("Enter bottom of range (int): ");
+
+
+    }
+}
