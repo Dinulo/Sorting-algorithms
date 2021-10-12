5f2a1fd5b60cdb3517c2b3bf14e11ef2cd608314
Dinulo <aleksandardinulovic99@gmail.com>
2021-10-12 17:13:33 +0200

    Update Main.java
    Added input validation and randomized array

--- java/bubblesort/src/Main.java
+++ java/bubblesort/src/Main.java
@@ -1,27 +1,49 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
-    Scanner inputReader = new Scanner(System.in);
+    static Scanner inputReader = new Scanner(System.in);
 
-    static int parseStringToInt(String inputString){
-        int outputInt;
-        Boolean flag = true;
-        for(int a = 0; a<inputString.length(); a++) {
-
+    static int parseStringToInt(String inputString) {
+        int outputInt = 0;
+        boolean flag = false;
+        while(!flag) {
+            try
+            {
+                outputInt = Integer.parseInt(inputString);
+                flag = true;
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println(inputString + " is not a valid integer. Please try again: ");
+                inputString = inputReader.nextLine();
+            }
         }
-
         return outputInt;
     }
 
     public static void main(String[] args) {
 	System.out.println("Bubblesort");
 
-
 	System.out.println("Enter array size (int): ");
+    int arraySize = parseStringToInt(inputReader.nextLine());
 
+    System.out.println("Enter bottom of range (int): ");
+    int rangeBottom = parseStringToInt(inputReader.nextLine());
 
-	System.out.println("Enter bottom of range (int): ");
+    System.out.println("Enter top of range (int): ");
+    int rangeTop = parseStringToInt(inputReader.nextLine());
+
+    //fill array with random numbers
+    Random rnd = new Random();
+    int[] numbers = new int[arraySize];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rnd.nextInt((rangeTop - rangeBottom) + rangeBottom);
+        }
+        System.out.println("Your random array: " + Arrays.toString(numbers));
 
 
     }
